615c8fb3b481207d3cea3bb05121ad66bb698c3f
IsabelleLof <104595550+IsabelleLof@users.noreply.github.com>
2022-05-19 23:38:49 +0200

    syntax for a childrens ap, conditinal a sleepin ap, wh ile loop

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ if(catsCounts => houndCounts) {
 //If both are true you can do your laundry on monday.
 //If you haven't booked the laundry on monday you can't do your laundry.
 
-const bookedMonday = false;
-const loggedIn = false;
+const bookedMonday = true;
+const loggedIn = true;
 if(bookedMonday && loggedIn) {
     console.log('you can do your laundry on monday')
 } else if(bookedMonday === true) {
@@ -26,3 +26,164 @@ if(bookedMonday && loggedIn) {
 } else {
     console.log('you can not do your laundry today beacause you have not booked and not logged in')
 }
+
+
+//"Is it night or day yet?" 
+
+//An app for children to process the realisation of what daytime means and nighttime means.
+//When does a child understand what night and day is, my son who is four, have started to say; 
+//Mom - it's dark outside, but he never says "it's light outside". What's happening in the night?
+//Section night-time
+//Wolf
+//"Mom, Dad, it's a dinasaur in our backyard."
+//People goes from the trains to their homes.
+//Other Moms and Dads are out. And other adults.
+//Nightbuses driving around.
+//Children are sleepin in their beds.
+//People are dreaming.
+
+//Small project 
+//What time is it?
+//When is it time for bed? 
+
+//let currentTime = 20;
+//const bedTime = 20;
+//const readingTime = 20;
+
+//const brushTime = 19;
+
+//let goToBed = currentTime === bedTime;
+//if(currentTime === bedTime) {
+    //console.log('it is bedtime')
+//} else { 
+    //console.log('it is not bedtime')
+//}
+
+//console.log(currentTime === bedTime); 
+
+//Boolean
+
+//If the lights is on the children wont sleep and if the lights are of they should sleep.
+//How do we creating the best way to get children to sleep?
+//
+
+const lightsOff = true;
+const readingTime = true;
+const changedClothes = true;
+const changedDiaper = true;
+const sleeping = false;
+
+//Create a conditinal that checks if the children will sleeping or not. 
+//Create variabels to check of what you need to do to get the children to sleep.
+//If the variabels are true they should but i they do not sleep, the last variabel is false.
+
+if(lightsOff && readingTime && changedClothes && changedDiaper === sleeping) {
+    console.log('the children are sleeping');
+} else {
+    console.log('the children is not able to sleep');
+}
+
+
+
+var counter1 = 0;
+
+while (counter1 < 1) {
+ console.log("<>--------------------------");
+ counter1++;
+}
+
+var counter2 = 0;
+
+while (counter2 < 1) {
+ console.log("--<>------------------------");
+ counter2++;
+}
+
+var counter3 = 0;
+
+while (counter3 < 1) {
+ console.log("----<>----------------------");
+ counter3++;
+}
+
+var counter4 = 0;
+
+while (counter4 < 1) {
+ console.log("------<>--------------------");
+ counter4++;
+}
+
+var counter5 = 0;
+
+while (counter5 < 1) {
+ console.log("--------<>------------------");
+ counter5++;
+}
+
+var counter6 = 0;
+
+while (counter6 < 1) {
+ console.log("----------<>----------------");
+ counter6++;
+}
+
+var counter7 = 0;
+
+while (counter7 < 1) {
+ console.log("------------<>--------------");
+ counter7++;
+}
+
+var counter8 = 0;
+
+while (counter8 < 1) {
+ console.log("--------------<>------------");
+ counter8++;
+}
+
+var counter9 = 0;
+
+while (counter9 < 1) {
+ console.log("----------------<>----------");
+ counter9++;
+}
+
+var counter10 = 0;
+
+while (counter10 < 1) {
+ console.log("------------------<>--------");
+ counter10++;
+}
+
+var counter11 = 0;
+
+while (counter11 < 1) {
+ console.log("--------------------<>------");
+ counter11++;
+}
+
+var counter12 = 0;
+
+while (counter12 < 1) {
+ console.log("----------------------<>----");
+ counter12++;
+}
+
+var counter13 = 0;
+
+while (counter13 < 1) {
+ console.log("------------------------<>--");
+ counter13++;
+}
+
+var counter14 = 0;
+
+while (counter14 < 1) {
+ console.log("--------------------------<>");
+ counter14++;
+}
+
+while (counter1 === counter14) {
+ counter14++;   
+}
+
